6de757228c9bbf907726b02de64182b452c9e0ba
Jannes Koehler <janneskoehler@gmail.com>
2012-01-08 17:47:41 +0100

    Change generator

--- lib/generators/install/install_generator.rb
+++ lib/generators/install/install_generator.rb
@@ -1,4 +1,4 @@
-class ChangeLog::InstallGenerator < Rails::Generators::Base
+class InstallGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
   
   def install
