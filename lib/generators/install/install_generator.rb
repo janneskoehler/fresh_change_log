35e7e8fe9090e6a702b18defa2e6938e4f8a40d3
Jannes Koehler <janneskoehler@gmail.com>
2012-01-09 08:02:18 +0100

    change generator

--- lib/generators/install/install_generator.rb
+++ lib/generators/install/install_generator.rb
@@ -1,9 +1,13 @@
-class InstallGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
+module FreshChangeLog
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path('../templates', __FILE__)
   
-  def install
-    generate('scaffold', 'ChangeLog::Protocol action:string model:string internal_id:integer')
-    generate('model', 'ChangeLog::Detail protocol:references attribute_name:string old_value:string')
+      def install
+        generate('scaffold', 'ChangeLog::Protocol action:string model:string internal_id:integer')
+        generate('model', 'ChangeLog::Detail protocol:references attribute_name:string old_value:string')
+      end
+  
+    end
   end
-  
 end
